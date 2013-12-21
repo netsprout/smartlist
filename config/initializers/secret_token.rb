db2be8684de76da94d432609b4be983b6d61cc88
Aaron Fry <afry@netsprout.com>
2013-12-20 23:35:48 -0800

    Initial commit for smartlist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartlist::Application.config.secret_key_base = '13621350572ce2e297bacf5eaab65f577b36dfde16801bbc383bc824ef52accac1e31f865ee9f42bce94ae1fdb235347fe699b6c55199cb503ccac20492e1333'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smartlist::Application.config.session_store :cookie_store, key: '_smartlist_session'
